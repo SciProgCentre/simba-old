7e17e8f702ac833940548dd6c34a1591c7658cd2
Mikhail Zelenyy <mihail.zelenyy@phystech.edu>
2020-02-28 08:29:46 +0300

    Add logger library

--- simba-physics/build.gradle.kts
+++ simba-physics/build.gradle.kts
@@ -12,7 +12,8 @@ dependencies {
     testImplementation(kotlin("test-junit"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
     compile("org.apache.commons:commons-math3:3.6.1")
-
+    compile ("io.github.microutils:kotlin-logging:1.5.4")
+    compile ("org.slf4j:slf4j-simple:1.7.5")
 }
 
 tasks.withType<KotlinCompile>().configureEach {
